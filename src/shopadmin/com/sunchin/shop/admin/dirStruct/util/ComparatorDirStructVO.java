cbe4fda04ace3a61593e41fb20131bc03f12dbc2
lenovo <lenovo@192.168.0.109>
2017-04-01 11:34:19 +0800

    s

--- src/shopadmin/com/sunchin/shop/admin/dirStruct/util/ComparatorDirStructVO.java
+++ src/shopadmin/com/sunchin/shop/admin/dirStruct/util/ComparatorDirStructVO.java
@@ -9,8 +9,8 @@ public class ComparatorDirStructVO implements Comparator<Object> {
 	public int compare(Object arg0, Object arg1) {
 		Map item_0 = (Map) arg0;
 		Map item_1 = (Map) arg1;
-		Integer i0 = Integer.parseInt(((Map)item_0.get("attributes")).get("cateOrder").toString());
-		Integer i1 = Integer.parseInt(((Map)item_1.get("attributes")).get("cateOrder").toString());
+		Integer i0 = Integer.parseInt(((Map)item_0.get("attributes")).get("order").toString());
+		Integer i1 = Integer.parseInt(((Map)item_1.get("attributes")).get("order").toString());
 		return i0.compareTo(i1);
 	}
 }
