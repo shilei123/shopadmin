4642ab88283291ee8798e032ed37abb56b21f56b
lenovo <lenovo@192.168.0.109>
2017-04-07 17:19:01 +0800

    新增两个枚举类

---
+++ src/shopadmin/com/sunchin/shop/admin/dict/AuditStsEnum.java
@@ -0,0 +1,36 @@
+package com.sunchin.shop.admin.dict;
+
+public enum AuditStsEnum {
+
+	WAIT_AUDIT("1", "待审核"),
+
+	PASS("2", "通过"),
+	
+	NO_PASS("1", "不通过");
+
+	private String code;
+
+	private String desc;
+
+	private AuditStsEnum(String code, String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+}
